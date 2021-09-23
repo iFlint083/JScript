a3a364daa576a13f9005ded8da3ca8e6eb069a12
Andrey <tashilin987@gmail.com>
2021-09-23 22:50:19 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//const ul = document.querySelector('ul');
+//ul.remove
+/* // рабочий код!!!!
+document.querySelector("input")
+  .addEventListener('change', (e) => {
+    console.log(e.currentTarget.value);
+ });
+*?
+
+
+/*button.addEventListener('click', () =>{
+    const li =document.createElement('li');
+    li.textContent = prompt('add something new to do');
+    ul.prepend(li);
+    var x = document.getElementById("myText");
+});
+function myFunction() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "text");
+    x.setAttribute("value", "Hello World!");
+    document.body.appendChild(x);
+
+  }
+
+/*const items = document.querySelectorAll('li');
+
+items.forEach(item => {
+    item.addEventListener('click', e =>{
+       //console.log(e.target);
+        //console.log(item);
+       // e.target.style.textDecoration = 'line-through';
+       e.target.remove();
+       e.stopPropagation();
+    })
+})
+ul.addEventListener('click', e => {
+    if(e.target.tagName === 'LI'){
+        e.target.remove();
+    }
+
+})*/
